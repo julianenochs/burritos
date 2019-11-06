2b91049bf9599c16b38bc9d59a22197c90f2bd34
Julian Enochs <julianenochs@yahoo.com>
2019-11-06 11:38:40 -0700

    Update

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -14,19 +14,3 @@ export const addOrders = (order) => {
   })
     .then(response => response.json(order))
 }
-
-//   fetch('http://localhost:3001/api/v1/users', {
-//     method: 'post',
-//     body: JSON.stringify({
-//       id: 3, name: "Juju", status: "sick", interests: "sleep"
-//     }),
-//       headers: {
-//   'Accept': 'application/json',
-//     'Content-Type': 'application/json'
-// },
-//     }).then(response => response.json())
-
-// fetch('http://localhost:3001/api/v1/users/3', {
-//   method: 'delete',
-// }).then(response => response.json())
-// }
